919acbb0bb101410d6c5b71fa01ee35f40629719
Greggles <gregory@wetransfer.com>
2021-11-26 02:12:21 +0100

    Implement stat range on enemies and increment

--- Assets/Scripts/Enemies/EnemyStats.cs
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 public class EnemyStats : ScriptableObject
 {
     public int level = 1;
+
+    public enum StatRange
+    {
+        S,
+        M,
+        L,
+        XL,
+        XXL,
+        XXX,
+        Nightmare,
+    }
     public int baseMaxHP = 1;
     public int baseAttackPower = 1;
     public int baseMoveSpeed = 1;
@@ -20,8 +31,47 @@ public class EnemyStats : ScriptableObject
     public int currencyDrop = 1;
     public int expYield = 1;
 
+    public StatRange statRange;
+
     void OnEnable()
     {
+        int multiplier = 1;
+        if (statRange == StatRange.S)
+        {
+            multiplier = 1;
+        }
+
+        if (statRange == StatRange.M)
+        {
+            multiplier = 2;
+        }
+
+        if (statRange == StatRange.L)
+        {
+            multiplier = 3;
+        }
+
+        if (statRange == StatRange.XL)
+        {
+            multiplier = 4;
+        }
+
+        if (statRange == StatRange.XXL)
+        {
+            multiplier = 5;
+        }
+
+        if (statRange == StatRange.XXX)
+        {
+            multiplier = 6;
+        }
+
+        if (statRange == StatRange.Nightmare)
+        {
+            multiplier = 7;
+        }
+
+
         maxHP = baseMaxHP;
         attackPower = baseAttackPower;
         moveSpeed = baseMoveSpeed;
@@ -30,13 +80,13 @@ public class EnemyStats : ScriptableObject
         currencyDrop = baseCurrencyDrop;
         expYield = baseExpYield;
 
-        maxHP = maxHP * level;
-        attackPower = attackPower * level;
-        moveSpeed = moveSpeed * level;
-        attackSpeed = attackSpeed * level;
-        defense = defense * level;
-        currencyDrop = currencyDrop * level;
-        expYield = expYield * level;
+        maxHP = maxHP * multiplier;
+        attackPower = attackPower * multiplier;
+        moveSpeed = moveSpeed * multiplier;
+        attackSpeed = attackSpeed * multiplier;
+        defense = defense * multiplier;
+        currencyDrop = currencyDrop * multiplier;
+        expYield = expYield * multiplier;
     }
 
 }
